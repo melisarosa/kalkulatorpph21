335910e70eada04fc0f10c01b6dc9a3f411971b3
Melisa Varani <m.varani@allegrodevelopment.com>
2022-04-27 22:15:25 +0700

    format currency, add rounding, create grid layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,86 @@
 <template>
   <div class="container">
     <b-form>
-      <b-form-group label="Memiliki NPWP">
-        <b-form-checkbox v-model="punyaNPWP"></b-form-checkbox>
-      </b-form-group>
-      <b-form-group label="Status Perkawinan">
-        <b-form-radio name="statusPerkawinan" v-model="statusPerkawinan" value="0">Tidak Kawin</b-form-radio>
-        <b-form-radio name="statusPerkawinan" v-model="statusPerkawinan" value="1">Kawin</b-form-radio>
-      </b-form-group>
-      <b-form-group label="Jumlah Tanggungan">
-        <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="0">0</b-form-radio>
-        <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="1">1</b-form-radio>
-        <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="2">2</b-form-radio>
-        <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="3">3</b-form-radio>
-      </b-form-group>
-      <b-form-group label="Gaji Pokok">
-        <b-form-input required type="number" placeholder="Gaji Pokok" v-model.number="gajiPokok"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Tunjangan">
-        <b-form-input required type="number" placeholder="Tunjangan" v-model.number="tunjangan"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Gaji yang dilaporkan di BPJS Ketenagakerjaan">
-        <b-form-input required type="number" placeholder="Gaji yang dilaporkan di BPJS Ketenagakerjaan" v-model.number="gajiBPJSTk"></b-form-input>
-      </b-form-group>
-      <b-form-group label="BPJS Kesehatan oleh Perusahaan (4%)">
-        <b-form-input readonly v-model="calculateBPJSKesP"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Resiko pekerjaan">
-        <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="0.24">Resiko sangat rendah</b-form-radio>
-        <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="0.54">Resiko rendah</b-form-radio>
-        <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="0.89">Resiko sedang</b-form-radio>
-        <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="1.27">Resiko tinggi</b-form-radio>
-        <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="1.74">Resiko sangat tinggi</b-form-radio>
-      </b-form-group>
-      <b-form-group :label="'Jaminan Kecelakaan Kerja (JKK) dibayarkan oleh Perusahaan (' + persentaseJkk + '%)'">
-        <b-form-input readonly v-model="calculateJKK"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Jaminan Kematian (JKM) dibayarkan oleh Perusahaan (0.3%)">
-        <b-form-input readonly v-model="calculateJKM"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Total Penghasilan Bruto per Tahun">
-        <b-form-input readonly v-model="calculateTotalPenghasilanBruto"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Biaya Jabatan (5%)">
-        <b-form-input readonly v-model="calculateBiayaJabatan"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Iuran Jaminan Hari Tua (JHT) dibayarkan oleh Karyawan (2%)">
-        <b-form-input readonly v-model="calculateJHT"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Iuran Jaminan Pensiun (JP) oleh Karyawan (1%)">
-        <b-form-input readonly v-model="calculateJP"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Total Pengurangan">
-        <b-form-input readonly v-model="calculateTotalPengurangan"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Total Penghasilan Neto per Tahun">
-        <b-form-input readonly v-model="calculateTotalPenghasilanNeto"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Penghasilan Tidak Kena Pajak (PTKP)">
-        <b-form-input readonly v-model="calculatePTKP"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Penghasilan Kena Pajak (PKP)">
-        <b-form-input readonly v-model="calculatePKP"></b-form-input>
-      </b-form-group>
-      <b-form-group label="PPh 21 terutang setahun">
-        <b-form-input readonly v-model="calculatePPh"></b-form-input>
-      </b-form-group>
-      <b-form-group label="PPh 21 terutang per bulan">
-        <b-form-input readonly v-model="calculatePPhBulanan"></b-form-input>
-      </b-form-group>
+      <div class="row">
+        <div class="col-md">
+          <b-form-group label="Memiliki NPWP">
+            <b-form-checkbox v-model="punyaNPWP"></b-form-checkbox>
+          </b-form-group>
+          <b-form-group label="Status Perkawinan">
+            <b-form-radio name="statusPerkawinan" v-model="statusPerkawinan" value="0">Tidak Kawin</b-form-radio>
+            <b-form-radio name="statusPerkawinan" v-model="statusPerkawinan" value="1">Kawin</b-form-radio>
+          </b-form-group>
+          <b-form-group label="Jumlah Tanggungan">
+            <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="0">0</b-form-radio>
+            <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="1">1</b-form-radio>
+            <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="2">2</b-form-radio>
+            <b-form-radio name="jumlahTanggungan" v-model="jumlahTanggungan" value="3">3</b-form-radio>
+          </b-form-group>
+          <b-form-group label="Gaji Pokok per bulan">
+            <b-form-input required type="number" placeholder="Gaji Pokok" v-model.number="gajiPokok"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Tunjangan">
+            <b-form-input required type="number" placeholder="Tunjangan" v-model.number="tunjangan"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Gaji yang dilaporkan di BPJS Ketenagakerjaan">
+            <b-form-input required type="number" placeholder="Gaji yang dilaporkan di BPJS Ketenagakerjaan" v-model.number="gajiBPJSTk"></b-form-input>
+          </b-form-group>
+          <b-form-group label="BPJS Kesehatan oleh Perusahaan (4%)">
+            <b-form-input readonly :value="formatCurrency(calculateBPJSKesP)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Resiko pekerjaan">
+            <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="0.24">Resiko sangat rendah</b-form-radio>
+            <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="0.54">Resiko rendah</b-form-radio>
+            <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="0.89">Resiko sedang</b-form-radio>
+            <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="1.27">Resiko tinggi</b-form-radio>
+            <b-form-radio name="persentaseJkk" v-model.number="persentaseJkk" value="1.74">Resiko sangat tinggi</b-form-radio>
+          </b-form-group>
+          <b-form-group :label="'Jaminan Kecelakaan Kerja (JKK) dibayarkan oleh Perusahaan (' + persentaseJkk + '%)'">
+            <b-form-input readonly :value="formatCurrency(calculateJKK)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Jaminan Kematian (JKM) dibayarkan oleh Perusahaan (0.3%)">
+            <b-form-input readonly :value="formatCurrency(calculateJKM)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Total Penghasilan Bruto per Bulan">
+            <b-form-input readonly  :value="formatCurrency(calculateTotalPenghasilanPerBulan)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Total Penghasilan Bruto per Tahun">
+            <b-form-input readonly  :value="formatCurrency(calculateTotalPenghasilanBruto)"></b-form-input>
+          </b-form-group>
+        </div>
+        <div class="col-md">
+          <b-form-group label="Biaya Jabatan (5%)">
+            <b-form-input readonly :value="formatCurrency(calculateBiayaJabatan)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Iuran Jaminan Hari Tua (JHT) dibayarkan oleh Karyawan (2%)">
+            <b-form-input readonly :value="formatCurrency(calculateJHT)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Iuran Jaminan Pensiun (JP) oleh Karyawan (1%)">
+            <b-form-input readonly :value="formatCurrency(calculateJP)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="BPJS Kesehatan oleh Karyawan (1%)">
+            <b-form-input readonly :value="formatCurrency(calculateBPJSKesK)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Total Pengurangan">
+            <b-form-input readonly :value="formatCurrency(calculateTotalPengurangan)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Total Penghasilan Neto per Tahun">
+            <b-form-input readonly :value="formatCurrency(calculateTotalPenghasilanNeto)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Penghasilan Tidak Kena Pajak (PTKP)">
+            <b-form-input readonly :value="formatCurrency(calculatePTKP)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Penghasilan Kena Pajak (PKP)">
+            <b-form-input readonly :value="formatCurrency(calculatePKP)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="PPh 21 terutang setahun">
+            <b-form-input readonly :value="formatCurrency(calculatePPh)"></b-form-input>
+          </b-form-group>
+          <b-form-group label="PPh 21 terutang per bulan">
+            <b-form-input readonly :value="formatCurrency(calculatePPhBulanan)"></b-form-input>
+          </b-form-group>
+        </div>
+      </div>
     </b-form>
   </div>
 </template>
@@ -99,13 +111,18 @@ export default {
       pphSebulan: 0
     }
   },
+  methods: {
+    formatCurrency(amount) {
+      return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount)
+    }
+  },
   computed: {
     calculateTotalGaji() {
       return this.gajiPokok + this.tunjangan
     },
     calculatePTKP() {
-      const ptkp = 54000000
-      const tambahan = 4500000
+      const ptkp = 54_000_000
+      const tambahan = 4_500_000
       const total = ptkp + this.jumlahTanggungan * tambahan
       if (this.statusPerkawinan == 1)
         return total + tambahan
@@ -113,23 +130,23 @@ export default {
         return total
     },
     calculateGajiBPJSKes() {
-      const limitGajiBPJSKes = 12000000
+      const limitGajiBPJSKes = 12_000_000
       if (this.calculateTotalGaji > limitGajiBPJSKes)
         return limitGajiBPJSKes
       else 
         return this.calculateTotalGaji
     },
     calculateBPJSKesP() {
-      return this.calculateGajiBPJSKes * 4/100
+      return Math.floor(this.calculateGajiBPJSKes * 4/100)
     },
     calculateBPJSKesK() {
-      return this.calculateGajiBPJSKes * 1/100
+      return Math.ceil(this.calculateGajiBPJSKes * 1/100)
     },
     calculateJKK() {
-      return this.calculateTotalGaji * this.persentaseJkk/100
+      return Math.ceil(this.calculateTotalGaji * this.persentaseJkk/100)
     },
     calculateJKM() {
-      return this.calculateTotalGaji * 0.3/100
+      return Math.ceil(this.calculateTotalGaji * 0.3/100)
     },
     calculateTotalPenghasilanPerBulan(){
       return (this.calculateTotalGaji + this.calculateBPJSKesP + this.calculateJKK + this.calculateJKM)
@@ -146,10 +163,10 @@ export default {
         return biayaJabatan
     },
     calculateJHT() {
-      return this.calculateTotalGaji * 2/100
+      return Math.ceil(this.calculateTotalGaji * 2/100)
     },
     calculateJP() {
-      return this.gajiPokok * 1/100
+      return (this.gajiPokok > 9_077_600 ? 9_077_600 : this.gajiPokok) * 1/100
     },
     calculateTotalPengurangan() {
       return this.calculateBiayaJabatan + (this.calculateJHT + this.calculateJP) * 12
@@ -158,16 +175,18 @@ export default {
       return this.calculateTotalPenghasilanBruto - this.calculateTotalPengurangan
     },
     calculatePKP() {
-      if (this.calculateTotalPenghasilanNeto <= this.calculatePTKP)
+      if (this.calculateTotalPenghasilanNeto <= this.calculatePTKP )
         return 0
       else
-        return this.calculateTotalPenghasilanNeto - this.calculatePTKP
+        return Math.floor((this.calculateTotalPenghasilanNeto - this.calculatePTKP) / 1000) *   1000
     },
     calculatePPh() {
-      const tarif = [5/100, 15/100, 25/100, 30/100]
+
+      const tarif = [5/100, 15/100, 25/100, 30/100, 35/100]
       const pkp = this.calculatePKP
-      const penghasilan = [50000000, 250000000, 500000000]
+      const penghasilan = [60_000_000, 250_000_000, 500_000_000, 5000_000_000]
       let pph = 0
+
       if (pkp <= penghasilan[0])
         pph = pkp * tarif[0] 
       else if (pkp > penghasilan[0] && pkp <= penghasilan[1])
@@ -187,7 +206,7 @@ export default {
       return pph
     },
     calculatePPhBulanan() {
-      return this.calculatePPh / 12
+      return Math.ceil(this.calculatePPh / 12)
     }
   }
 }
